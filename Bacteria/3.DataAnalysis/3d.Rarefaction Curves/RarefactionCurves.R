45b9d34cc5e9e1484b386c24bfde62d85cdb1b0d
Fotisbs <87061822+Fotisbs@users.noreply.github.com>
2025-11-07 13:53:19 +0200

    Update RarefactionCurves.R

--- Bacteria/3.DataAnalysis/3d.Rarefaction Curves/RarefactionCurves.R
+++ Bacteria/3.DataAnalysis/3d.Rarefaction Curves/RarefactionCurves.R
@@ -1,8 +1,9 @@
 ##Rarefaction Curves##
 bacteria_vinification_Annotated <- readRDS(".................")
 
-MUST_Bacteria_Vidiano_2019 <- Bacteria_vinification_Annotated
+MUST_Bacteria_Vidiano_2019 <- bacteria_vinification_Annotated
 
 a <- data.frame(t(otu_table(MUST_Bacteria_Vidiano_2019)))
+
 
 rarecurve(a, step=50, cex=0.5, label = F, xlim=c(0,650), ylim =c(0,20))
