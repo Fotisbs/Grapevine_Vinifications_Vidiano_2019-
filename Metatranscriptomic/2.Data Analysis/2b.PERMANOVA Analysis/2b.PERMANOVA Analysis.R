3e3146ffc55549504f189e58a9824162bc51cdba
Fotisbs <87061822+Fotisbs@users.noreply.github.com>
2026-07-01 15:51:21 +0300

    Update 2b.PERMANOVA Analysis.R

--- Metatranscriptomic/2.Data Analysis/2b.PERMANOVA Analysis/2b.PERMANOVA Analysis.R
+++ Metatranscriptomic/2.Data Analysis/2b.PERMANOVA Analysis/2b.PERMANOVA Analysis.R
@@ -1,14 +1,31 @@
 ##PERMANOVA##
-##PERMANOVA Variables (perm. 999)##
-fungi_vinification_Annotated <- readRDS(".................")
+#Build the object with transcriptomic data (80046 Genes, counts)
 
-MUST_Fungi_Vidiano_2019 <- fungi_vinification_Annotated
+cts <- read.table (file = "Genes.txt", header = TRUE, sep = "\t",quote = "", stringsAsFactors = FALSE, row.names = 1)
+View(data.frame(cts))
+
+TheDesign <- read.table (file = "ExperimentalDesign.txt", header = TRUE, sep = "\t",quote = "", stringsAsFactors = FALSE, row.names = 1)
+View(data.frame(TheDesign))
+
+ps_Transcript
+
+ps_Transcript <- phyloseq(otu_table(cts, taxa_are_rows=FALSE), sample_data(TheDesign))
 
 ##Transform RA 100%
-MUST_Fungi_Vidiano_2019_100 <- transform_sample_counts(MUST_Fungi_Vidiano_2019, function(OTU) 100*OTU/sum(OTU))
+ps_Transcript_100 <- transform_sample_counts(ps_Transcript, function(OTU) 100*OTU/sum(OTU))
 
-MUST_Fungi_Vidiano_2019_100 #(Transformation 100%)
+taxa_total <- taxa_sums(ps_Transcript_100)
 
-mypermanova_MUST_Fungi_Vidiano_2019_100 <- adonis2(MUST_Fungi_Vidiano_2019_100@otu_table ~ vinification + stage2, method = "bray", data = data.frame(MUST_Fungi_Vidiano_2019_100@sam_data))
+#Filter Genes and keep those contributing at least 0.1%
+ps_Transcript_100_filtered <- prune_taxa(taxa_total >= 0.1, ps_Transcript_100)
 
-write.table(data.frame(mypermanova_MUST_Fungi_Vidiano_2019_100), file="mypermanova_MUST_Fungi_Vidiano_2019_100.txt", quote = F,col.names = NA, sep="\t")
+ps_Transcript_100_filtered
+
+mypermanova_ps_Transcript_100_filtered <- adonis2(
+  ps_Transcript_100_filtered@otu_table ~ Vinification + Stage,
+  data = data.frame(ps_Transcript_100_filtered@sam_data),
+  method = "bray",
+  by = "margin"
+)
+
+write.table(data.frame(mypermanova_ps_Transcript_100_filtered), file="mypermanova_ps_Transcript_100_filtered.txt", quote = F,col.names = NA, sep="\t")
