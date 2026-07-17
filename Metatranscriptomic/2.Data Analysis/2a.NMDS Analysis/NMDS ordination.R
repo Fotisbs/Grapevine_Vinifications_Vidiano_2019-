223045967d72d22c9e8dcf6ad91fcfb54bcf7cdc
Fotisbs <87061822+Fotisbs@users.noreply.github.com>
2026-07-17 14:29:17 +0300

    Update NMDS ordination.R

--- Metatranscriptomic/2.Data Analysis/2a.NMDS Analysis/NMDS ordination.R
+++ Metatranscriptomic/2.Data Analysis/2a.NMDS Analysis/NMDS ordination.R
@@ -16,8 +16,32 @@ library(vegan)
 library(RColorBrewer)
 
 ## =========================================================
-## 1. Check the phyloseq object
+## 1. Build and Check the phyloseq object
 ## =========================================================
+
+#Build the object with transcriptomic data (80046 Genes, counts)
+
+cts <- read.table (file = "FinalNames.txt", header = TRUE, sep = "\t",quote = "", stringsAsFactors = FALSE, row.names = 1)
+View(data.frame(cts))
+
+TheDesign <- read.table (file = "ExperimentalDesign.txt", header = TRUE, sep = "\t",quote = "", stringsAsFactors = FALSE, row.names = 1)
+View(data.frame(TheDesign))
+
+ps_Transcript
+
+ps_Transcript <- phyloseq(otu_table(cts, taxa_are_rows=FALSE), sample_data(TheDesign))
+
+##Transform RA 100%
+ps_Transcript_100 <- transform_sample_counts(ps_Transcript, function(OTU) 100*OTU/sum(OTU))
+
+taxa_total <- taxa_sums(ps_Transcript_100)
+
+#Filter Genes and keep those contributing at least 0.1%
+ps_Transcript_100_filtered <- prune_taxa(taxa_total >= 0.1, ps_Transcript_100)
+
+ps_Transcript_100_filtered
+
+
 
 ps_Transcript_100_filtered
 
